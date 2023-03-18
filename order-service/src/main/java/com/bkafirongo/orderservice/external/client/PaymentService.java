fbe2a216477b6bddbd308a0d774359373db57694
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2023-03-18 16:25:24 +0100

    add limit rate on api call, resilience when a service goes donw

--- order-service/src/main/java/com/bkafirongo/orderservice/external/client/PaymentService.java
+++ order-service/src/main/java/com/bkafirongo/orderservice/external/client/PaymentService.java
@@ -1,14 +1,20 @@
 package com.bkafirongo.orderservice.external.client;
 
+import com.bkafirongo.orderservice.exception.CustomerException;
 import com.bkafirongo.orderservice.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CircuitBreaker(name ="external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payments")
 public interface PaymentService {
 
     @PostMapping
     ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
 
+    default void fallback(Exception e){
+        throw  new CustomerException("Payment service is down","UNAIVALABLE", 500);
+    }
 }
--- order-service/src/main/java/com/bkafirongo/orderservice/external/client/ProductService.java
+++ order-service/src/main/java/com/bkafirongo/orderservice/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.bkafirongo.orderservice.external.client;
 
+import com.bkafirongo.orderservice.exception.CustomerException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name ="external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/products")
 public interface ProductService {
 
@@ -14,4 +17,7 @@ public interface ProductService {
             @PathVariable("id") Long productId,
             @RequestParam("quantity") Long quantity);
 
+    default void fallback(Exception e){
+        throw  new CustomerException("Product service is down","UNAIVALABLE", 500);
+    }
 }
